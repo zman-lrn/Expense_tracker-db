8ca206e1ba7d8374a6948f2fd4bb34643140a65a
zman-lrn <zemnemenber@gmail.com>
2025-11-20 17:54:15 +0300

    api commit19

--- knexfile.js
+++ knexfile.js
@@ -27,19 +27,6 @@
 require("dotenv").config();
 
 module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl:
-        process.env.NODE_ENV === "production"
-          ? { rejectUnauthorized: false }
-          : false,
-    },
-    migrations: {
-      directory: "./migrations",
-    },
-  },
   production: {
     client: "pg",
     connection: {

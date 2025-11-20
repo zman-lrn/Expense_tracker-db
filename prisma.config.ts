5197a6d4a7c6c924a9cc202fce1f83e0514d3542
zman-lrn <zemnemenber@gmail.com>
2025-11-20 12:33:24 +0300

    api commit6

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL, // your deployed DB URL
-    },
-  },
-});

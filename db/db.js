b9378c618d723111e21e35e87c57c39e04008de2
zman-lrn <zemnemenber@gmail.com>
2025-11-20 08:16:16 +0300

    api commit2

--- db/db.js
+++ db/db.js
@@ -3,13 +3,7 @@ require("dotenv").config();
 
 const db = knex({
   client: "pg",
-  connection: process.env.DATABASE_URL || {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
+  connection: process.env.DATABASE_URL,
 });
 
 module.exports = db;
